91331b1201ae2c7bc2a627136116b19bf97d63f2
unknown <fgxmendoza23@gmail.com>
2021-09-07 14:01:49 +0800

    forming the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text(''),
+      home: SelectBranch(),
     ));
+
+class SelectBranch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // no appbar
+      body: Center(
+        child: Text(
+          'Select Branch',
+          style: TextStyle(
+            //fontSize: ,
+            //fontWeight: FontWeight.bold,
+            //letterSpacing: 2.0,
+            //color: Colors.grey[600],
+            fontFamily: 'Nunito',
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('CDO'),
+        backgroundColor: Color.fromRGBO(243, 233, 211, 1),
+      ),
+    );
+  }
+}
