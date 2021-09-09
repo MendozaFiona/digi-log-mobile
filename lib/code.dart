9b902eb79168802ad272111b3f0f3e004a6f974b
unknown <fgxmendoza23@gmail.com>
2021-09-10 06:35:13 +0800

    added code references

---
+++ lib/code.dart
@@ -0,0 +1,50 @@
+/*
+# FOR IMAGES:
+# make sure to put in pubspec.yaml
+# in the body: Center: child
+
+Image(
+  image: NetworkImage('url')
+  or
+  image: AssetImage('path')
+),
+
+#shortened
+Image.asset or Image.network
+
+# FOR ICONS:
+
+Icon(
+  Icons.airport_shuttle,
+  color: Color.fromRGBO(),
+  size: 50.0
+)
+
+# For Buttons
+
+or FlatButton - deprecated use TextButton
+RaisedButton( - deprecated use ElevatedButton
+  onPressed: () {},
+  child: Text(''),
+  color: Color.fromRGBO
+)
+
+# if inside has icon
+
+RaisedButton.icon( -use elevated
+  onPressed: () {},
+  icon: Icon(
+    Icons.mail
+  ),
+  Label: Text(''),
+  color: ,
+)
+
+# icon button
+IconButton(
+  onPressed() {},
+  icon: Icon(Icons.dadada),
+  color: Color.,
+)
+
+*/
--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,11 @@ class SelectBranch extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // no appbar
-      body: Center(
-        child: Text(
-          'Select Branch',
-          style: TextStyle(
-            //fontSize: ,
-            //fontWeight: FontWeight.bold,
-            //letterSpacing: 2.0,
-            //color: Colors.grey[600],
-            fontFamily: 'Nunito',
-          ),
-        ),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.fromLTRB(5.0, 7.0, 3.0, 4.0),
+        color: Colors.blue[400],
+        child: Text(''),
       ),
 
       floatingActionButton: FloatingActionButton(
@@ -30,3 +24,18 @@ class SelectBranch extends StatelessWidget {
     );
   }
 }
+
+/*
+Center(
+  child: Text(
+    'Select Branch',
+    style: TextStyle(
+      //fontSize: ,
+      //fontWeight: FontWeight.bold,
+      //letterSpacing: 2.0,
+      //color: Colors.grey[600],
+      fontFamily: 'Nunito',
+    ),
+  ),
+),
+*/
