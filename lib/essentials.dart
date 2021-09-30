a5eb31e1097e4e57446a92c1d0fafa4790854122
unknown <fgxmendoza23@gmail.com>
2021-09-30 16:08:02 +0800

    replaced to push instead of popandpush

--- lib/essentials.dart
+++ lib/essentials.dart
@@ -26,20 +26,18 @@ SafeArea mapBody(_title) {
                   )),
             ),
           )),
-      
       Flexible(
           flex: 11,
           child: Center(
               child: Container(
-                color: Colors.red,
-      ))),
-      
+            color: Colors.red,
+          ))),
       Flexible(
           flex: 2,
           child: Center(
               child: Container(
-                color: Colors.green,
-      )))
+            color: Colors.green,
+          )))
     ]),
   );
 }
@@ -75,7 +73,6 @@ SafeArea bodyFormat(context, _title, _textList, _functOption) {
               child: Container(
                   margin: EdgeInsets.symmetric(vertical: 40, horizontal: 10),
                   padding: EdgeInsets.symmetric(horizontal: 40),
-
                   decoration: BoxDecoration(
                     color: Color.fromRGBO(255, 255, 255, 0.15),
                     border: Border.all(
@@ -83,65 +80,58 @@ SafeArea bodyFormat(context, _title, _textList, _functOption) {
                     ),
                     borderRadius: BorderRadius.all(Radius.circular(20)),
                   ),
-                  
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       //Spacer()
-                      if(_functOption == 'optSect')
+                      if (_functOption == 'optSect')
                         optionSection(context, _textList),
-                      if(_functOption == 'fillForm')
+                      if (_functOption == 'fillForm')
                         fillupForm(context, _textList)
-
                     ],
                   ))))
     ]),
   );
 }
 
-Column optionSection(context, _optionList){
-  return Column(
-    children: [
-      for (var options in _optionList)
-        optionsLight(context, options.toString()),
-    ]);
+Column optionSection(context, _optionList) {
+  return Column(children: [
+    for (var options in _optionList) optionsLight(context, options.toString()),
+  ]);
 }
 
-Column fillupForm(context, _formTitle){
-  return Column(children: [      
+Column fillupForm(context, _formTitle) {
+  return Column(
+    children: [
       Text(_formTitle,
-        style: TextStyle(
-          fontSize: 23,
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Roboto',
-        )),
+          style: TextStyle(
+            fontSize: 23,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Roboto',
+          )),
       SizedBox(height: 15),
-
       Container(
         height: 40,
         width: 250,
         child: TextFormField(
           style: TextStyle(
-            fontSize: 20.0, 
+            fontSize: 20.0,
           ),
           decoration: InputDecoration(
             filled: true,
             fillColor: Colors.white,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0)
-            ),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
             focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color:Color.fromRGBO(25, 24, 81, 1),
-              ),
-              borderRadius: BorderRadius.circular(10.0)
-            ),
+                borderSide: BorderSide(
+                  color: Color.fromRGBO(25, 24, 81, 1),
+                ),
+                borderRadius: BorderRadius.circular(10.0)),
             contentPadding: EdgeInsets.symmetric(horizontal: 2, vertical: 3),
           ),
         ),
       ),
-
       SizedBox(height: 15),
       optionsLight(context, 'Submit')
     ],
@@ -151,14 +141,12 @@ Column fillupForm(context, _formTitle){
 ElevatedButton optionsLight(context, _optionText) {
   return ElevatedButton(
     onPressed: () {
-      if(_optionText == 'Cagayan de Oro'){
-        Navigator.popAndPushNamed(context,'/visitUSTP');
-      }
-      else if(_optionText == 'View Map'){
-        Navigator.popAndPushNamed(context,'/inputName');
-      }
-      else if(_optionText == 'Submit'){
-        Navigator.popAndPushNamed(context,'/mapNav'); 
+      if (_optionText == 'Cagayan de Oro') {
+        Navigator.pushNamed(context, '/visitUSTP');
+      } else if (_optionText == 'View Map') {
+        Navigator.pushNamed(context, '/inputName');
+      } else if (_optionText == 'Submit') {
+        Navigator.pushNamed(context, '/mapNav');
       }
     },
     child: Text(_optionText,
