423008ccebb7b0e8ac20d37bdfd6c59bb4e424f7
unknown <fgxmendoza23@gmail.com>
2021-09-30 16:53:13 +0800

    popped if denied service

--- lib/main.dart
+++ lib/main.dart
@@ -83,20 +83,28 @@ class _MapNavState extends State<MapNav> {
     LocationData _locationData;
 
     _serviceEnabled = await location.serviceEnabled();
+
     if (!_serviceEnabled) {
       _serviceEnabled = await location.requestService();
       if (!_serviceEnabled) {
-        return;
+        return Navigator.pop(context);
       }
     }
 
     _permissionGranted = await location.hasPermission();
+
     if (_permissionGranted == PermissionStatus.denied) {
       _permissionGranted = await location.requestPermission();
       if (_permissionGranted != PermissionStatus.granted) {
         return;
       }
     }
+
+    /*ValueListenableBuilder<bool>(
+      valueListenable: ,
+      builder: ,
+    
+    );*/
 
     _locationData = await location.getLocation();
   }
